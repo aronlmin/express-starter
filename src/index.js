e7a696d1fa6e01a3533a5e903ac8284660d3dda5
Aron Lilland <aronlmin@gmail.com>
2019-06-08 16:20:41 -0700

    comments

--- src/index.js
+++ src/index.js
@@ -15,8 +15,11 @@ const device = require('express-device')
 const logger = require('./lib/logger')
 const compression = require('compression')
 
+// gzip all responses
 app.use(compression())
+// obtain the type of device making the request
 app.use(device.capture())
+// log the route / device information
 app.use((req, res, next) => {
   req.start = moment()
   logger.log('info', `[appEntry] ${req.originalUrl}`)
@@ -25,20 +28,23 @@ app.use((req, res, next) => {
   logger.log('debug', `[appEntry] ** IP Address ${req.ip} / ${req.ips}`)
   next()
 })
+// log http traffic
 let accessLogStream = rfs('access.log', {
   interval: '1d', // rotate daily
   path: path.join(__dirname, 'log')
 })
 app.use(morgan('combined', { stream: accessLogStream }))
 if (!isProduction) app.use(morgan('dev'))
+// enable requests from all places
 app.use(cors())
+// parse requests as json
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-// DATABASE
+// load models
 require('./models')
 
-// require routes
+// load routes
 app.use('/', require('./routes'))
 
 const port = process.env.PORT
@@ -49,5 +55,7 @@ server.listen(port, () => {
   logger.log('debug', `[appEntry] ** Listening on: tcp://localhost:${port}`)
 })
 
+// allow chai tests to open connections
 module.exports = app
+// allow chai tests to close connections
 module.exports.stop = () => server.close()
